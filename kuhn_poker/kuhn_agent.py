84494458453825bb48d7fa9fdb6376c9e8803438
Jakub Petriska <jakubpetriska@gmail.com>
2017-08-24 22:54:45 +0200

    Rename stuff

--- kuhn_poker/kuhn_agent.py
+++ kuhn_poker/kuhn_agent.py
@@ -1,9 +1,10 @@
 import random
+import sys
 
 import acpc_python_client as acpc
 
 
-class PlayingAgent(acpc.Agent):
+class KuhnAgent(acpc.Agent):
     def __init__(self):
         super().__init__()
         self.actions = [acpc.ActionType.FOLD, acpc.ActionType.CALL, acpc.ActionType.RAISE]
@@ -49,3 +50,12 @@ class PlayingAgent(acpc.Agent):
 
     def on_game_finished(self, game, match_state):
         pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage {game_file_path} {dealer_hostname} {dealer_port}")
+        sys.exit(1)
+
+    client = acpc.Client(sys.argv[1], sys.argv[2], sys.argv[3])
+    client.play(KuhnAgent())
