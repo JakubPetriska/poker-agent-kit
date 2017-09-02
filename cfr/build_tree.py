770e19d536eb2bd1c3cbf2031fafcec0f657a00c
Jakub Petriska <jakubpetriska@gmail.com>
2017-09-03 00:20:54 +0200

    Add script to launch general CFR training

---
+++ cfr/build_tree.py
@@ -0,0 +1,30 @@
+from cfr.game_tree import HoleCardNode, ActionNode, TerminalNode
+
+
+def build_game_tree(game):
+    if not game.get_num_players() == 2:
+        raise AttributeError('Only games with 2 players supported')
+
+    # TODO obey the game, this is just hardcoded Kuhn tree for 2 players
+    root = HoleCardNode(None)
+    for card in range(1, 4):
+        first_action_node = ActionNode(root, 0)
+        root.set_child(card, first_action_node)
+
+        second_action_node_call = ActionNode(first_action_node, 1)
+        first_action_node.set_child(1, second_action_node_call)
+
+        second_action_node_call.set_child(1, TerminalNode(second_action_node_call, [1, 1]))
+
+        third_action_node_call_raise = ActionNode(second_action_node_call, 0)
+        second_action_node_call.set_child(2, third_action_node_call_raise)
+
+        third_action_node_call_raise.set_child(0, TerminalNode(second_action_node_call, [1, 2]))
+        third_action_node_call_raise.set_child(1, TerminalNode(second_action_node_call, [2, 2]))
+
+        second_action_node_raise = ActionNode(first_action_node, 1)
+        first_action_node.set_child(2, second_action_node_raise)
+
+        second_action_node_raise.set_child(0, TerminalNode(second_action_node_raise, [2, 1]))
+        second_action_node_raise.set_child(1, TerminalNode(second_action_node_raise, [2, 2]))
+    return None
---
+++ train.py
@@ -0,0 +1,66 @@
+import sys
+
+import acpc_python_client as acpc
+from tqdm import tqdm  # TODO make import optional
+
+from cfr.build_tree import build_game_tree
+from cfr.cfr import Cfr
+from cfr.game_tree import HoleCardNode, ActionNode
+
+
+def _action_to_str(action):
+    if action == 0:
+        return 'f'
+    elif action == 1:
+        return 'c'
+    else:
+        return 'r'
+
+
+def _get_strategy_lines(lines, node, prefix=''):
+    if type(node) == HoleCardNode:
+        for card, child_node in node.children.items:
+            _get_strategy_lines(lines, child_node, '%s%s:' % (prefix, str(card)))
+    elif type(node) == ActionNode:
+        node_strategy_str = ' '.join([str(prob) for prob in node.strategy])
+        lines.append('%s: %s\n' % (prefix, node_strategy_str))
+        for action, child_node in node.children.items:
+            _get_strategy_lines(lines, child_node, '%s%s:' % (prefix, _action_to_str(action)))
+
+
+def write_strategy(game_tree, output_path):
+    print('Obtaining strategy entries')
+    with tqdm(total=100) as progress:
+        strategy_file_lines = []
+        _get_strategy_lines(strategy_file_lines, game_tree)
+        progress.update(100)
+
+    print('Sorting strategy file')
+    with tqdm(total=100) as progress:
+        strategy_file_lines_sorted = sorted(strategy_file_lines)
+        progress.update(100)
+
+    print('Writing strategy file')
+    with tqdm(total=100) as progress:
+        with open(output_path, 'w') as file:
+            for line in strategy_file_lines_sorted:
+                file.write(line)
+        progress.update(100)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage {game_file_path} {iterations} {strategy_output_path}")
+        sys.exit(1)
+
+    game = acpc.read_game_file(sys.argv[1])
+    game_tree = build_game_tree(game)
+
+    iterations = int(sys.argv[2])
+
+    output_path = sys.argv[3]
+
+    cfr = Cfr(game.get_num_players(), game_tree)
+    cfr.train(iterations)
+
+    write_strategy(game_tree, output_path)
