ab247f37b72bf833dfb32c93d01e6889642b109e
Jakub Petriska <jakubpetriska@gmail.com>
2017-09-03 22:49:56 +0200

    Add str method for debugging to node

--- cfr/game_tree.py
+++ cfr/game_tree.py
@@ -10,6 +10,27 @@ class Node:
     def set_child(self, key, child):
         self.children[key] = child
 
+    def __str__(self):
+        if not self.parent:
+            return ''
+        parent_str = str(self.parent)
+        parents_children = list(filter(lambda item: item[1] == self, self.parent.children.items()))
+        if len(parents_children) == 0:
+            raise RuntimeError('Parent does have this node as a child')
+        child_key = parents_children[0][0]
+        if type(self.parent) == HoleCardNode:
+            child_key = str(child_key) + ':'
+            if parent_str and not parent_str.startswith(':'):
+                child_key = ':' + child_key
+        elif type(self.parent) == ActionNode:
+            if child_key == 0:
+                child_key = 'f'
+            elif child_key == 1:
+                child_key = 'c'
+            elif child_key == 2:
+                child_key = 'r'
+        return parent_str + child_key
+
 
 class TerminalNode(Node):
     def __init__(self, parent, pot_commitment):
