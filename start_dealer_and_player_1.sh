6fc4220e8fda923a7b866c27f028f18aae3e4853
Jakub Petriska <jakubpetriska@gmail.com>
2017-09-04 17:24:10 +0200

    Add support for more than 2 players to script

--- start_dealer_and_player_1.sh
+++ start_dealer_and_player_1.sh
@@ -10,6 +10,16 @@ ACPC_CLIENT_DIR='../acpc-python-client'
 SCRIPT_DIR=$(pwd)
 GAME_DIR_PATH=${SCRIPT_DIR}/$1
 GAME_NAME="$(basename ${GAME_DIR_PATH})"
+GAME_FILE_PATH="${GAME_DIR_PATH}/${GAME_NAME}.game"
+
+# Get number of players
+NUM_PLAYERS=$(cat ${GAME_FILE_PATH} | grep numPlayers | sed 's/\s*numPlayers\s*=\s*\([0-9]*\)\s*/\1/')
+NUM_RANDOM_PLAYERS=$((${NUM_PLAYERS} - 1))
+PLAYERS=""
+for i in `seq 1 ${NUM_RANDOM_PLAYERS}`;
+do
+  PLAYERS="${PLAYERS} Random_${i} "${GAME_DIR_PATH}/agent_random.${GAME_NAME}.sh""
+done
 
 LOGS_DIR="${SCRIPT_DIR}/logs"
 
@@ -21,10 +31,10 @@ cd ${ACPC_CLIENT_DIR}
 
 TIMESTAMP=$(date +%s)
 
-./scripts/_start_dealer_and_player_1.pl \
+eval ./scripts/_start_dealer_and_player_1.pl \
   "${LOGS_DIR}/${GAME_NAME}" \
-  "${GAME_DIR_PATH}/${GAME_NAME}.game" \
+  ${GAME_FILE_PATH} \
   1000 \
   ${TIMESTAMP} \
-  Random "${GAME_DIR_PATH}/agent_random.${GAME_NAME}.sh" \
+  ${PLAYERS} \
   CFR_trained
