f2247525e0054153a50a7fdc210b11f0a30c7a89
Jakub Petriska <jakubpetriska@gmail.com>
2018-04-18 11:33:36 +0200

    Add best response test to main test suite

--- test/main.py
+++ test/main.py
@@ -4,10 +4,12 @@ from unittest import TestSuite
 from test.hand_evaluation_tests import HandEvaluationTests
 from test.exploitability_tests import ExploitabilityTests
 from test.cfr_tests import CfrTests
+from test.best_response_game_value_tests import BestResponseGameValueTests
 
 test_classes = [
     HandEvaluationTests,
     ExploitabilityTests,
+    BestResponseGameValueTests,
     CfrTests
 ]
 
