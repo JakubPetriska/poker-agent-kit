d84c09e3be3c3a63e7f1550cfde752462e69ea28
Jakub Petriska <jakubpetriska@gmail.com>
2018-04-19 18:05:32 +0200

    Add more utils tests

--- test/utils_tests.py
+++ test/utils_tests.py
@@ -1,10 +1,30 @@
 import unittest
 
-from tools.utils import flatten
+from tools.utils import flatten, intersection, is_unique
 
 
 class UtilsTests(unittest.TestCase):
     def test_flatten(self):
-        data = [[1, 2], [3], [4, 5]]
-        flattened = flatten(*data)
-        self.assertEqual(flattened, [1, 2, 3, 4, 5])
+        self.assertEqual(
+            flatten([1, 2], [3], [4, 5]),
+            [1, 2, 3, 4, 5])
+
+    def test_intersection_empty(self):
+        self.assertEqual(
+            intersection([1, 2], [3, 4]),
+            set([]))
+
+    def test_intersection_non_empty(self):
+        self.assertEqual(
+            intersection([1, 2, 3, 4], [2, 4]),
+            set([2, 4]))
+
+    def test_is_unique_true(self):
+        self.assertEqual(
+            is_unique((1, 2), [3], [4, 5]),
+            True)
+
+    def test_is_unique_false(self):
+        self.assertEqual(
+            is_unique([1, 2], [3], [3, 4, 5]),
+            False)
