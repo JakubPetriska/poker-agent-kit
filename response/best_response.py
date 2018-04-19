Fix best response calculation, update CFR correctness tests

response/best_response.py
@@ -26,86 +26,98 @@ class BestResponse:
         best_response = game_tree_builder.build_tree()
 
         for position in range(2):
-            self._get_exploitability(position, best_response, strategy, [], [])
+            self._get_exploitability(position, best_response, [strategy], [1], [], [], [])
 
         return best_response
 
-    def _get_terminal_node_player_utility(self, player_position, node, best_response_cards, board_cards):
-        parent_action = list(filter(lambda item: item[1] == node, node.parent.children.items()))[0][0]
-        if parent_action == 0:
-            player_folded = node.parent.player
-            pot_amount = np.sum(node.pot_commitment)
-            return -node.pot_commitment[player_position] + \
-                (pot_amount if player_position != player_folded else 0)
-
-
-        deck = filter(
-            lambda card: card not in best_response_cards and card not in board_cards,
-            generate_deck(self.game))
-        num_hole_cards = self.game.get_num_hole_cards()
-
-        player_value_sum = 0
-        num_player_values = 0
-        for player_cards in itertools.combinations(deck, num_hole_cards):
-            hands = [player_cards, best_response_cards]
-            winners = get_winners(hands)
-            winner_count = len(winners)
-            pot_amount = np.sum(node.pot_commitment)
-            per_winner_value = pot_amount / winner_count
-            player_value_sum += -node.pot_commitment[player_position] + \
-                (per_winner_value if 0 in winners else 0)
-            num_player_values += 1
-        return player_value_sum / num_player_values
-
     def _get_exploitability(
-            self, player_position, best_response_node, player_possible_nodes, best_response_cards, board_cards):
+            self,
+            player_position,
+            best_response_node,
+            player_nodes,
+            player_nodes_reach_proabilities,
+            player_cards,
+            best_response_cards,
+            board_cards):
+
         if isinstance(best_response_node, TerminalNode):
-            return self._get_terminal_node_player_utility(player_position, best_response_node, best_response_cards, board_cards)
+            parent_action = list(filter(lambda item: item[1] == best_response_node, best_response_node.parent.children.items()))[0][0]
+            if parent_action == 0:
+                player_folded = best_response_node.parent.player
+                pot_amount = np.sum(best_response_node.pot_commitment)
+                return -best_response_node.pot_commitment[player_position] + \
+                    (pot_amount if player_position != player_folded else 0)
+
+            player_value_sum = 0
+            for i, cards in enumerate(player_cards):
+                hands = [cards, best_response_cards]
+                winners = get_winners(hands)
+                winner_count = len(winners)
+                pot_amount = np.sum(best_response_node.pot_commitment)
+                per_winner_value = pot_amount / winner_count
+                player_value = -best_response_node.pot_commitment[player_position] + \
+                    (per_winner_value if 0 in winners else 0)
+                player_value_sum += player_value * player_nodes_reach_proabilities[i]
+            return player_value_sum
+
+
         elif isinstance(best_response_node, HoleCardsNode):
             player_values_sum = 0
             for cards in best_response_node.children:
-                new_player_cards = flatten(best_response_cards, cards)
-                next_player_possible_nodes = []
+                new_bets_response_cards = flatten(best_response_cards, cards)
+                new_player_nodes = []
+                new_player_cards = []
                 for other_cards in best_response_node.children:
-                    if len(intersection(cards, other_cards)) == 0 \
-                        and len(intersection(cards, board_cards)) == 0:
-                        next_player_possible_nodes.append(player_possible_nodes.children[other_cards])
+                    if len(intersection(cards, other_cards)) == 0 and len(intersection(cards, board_cards)) == 0:
+                        new_player_nodes.append([node.children[other_cards] for node in player_nodes])
+                        new_player_cards.append(other_cards)
+                new_player_nodes = flatten(*new_player_nodes)
+                new_player_nodes_reach_probabilities = np.array([1 / len(new_player_nodes)] * len(new_player_nodes))
+
                 player_values_sum += self._get_exploitability(
                     player_position,
                     best_response_node.children[cards],
-                    next_player_possible_nodes,
+                    new_player_nodes,
+                    new_player_nodes_reach_probabilities,
                     new_player_cards,
+                    new_bets_response_cards,
                     board_cards)
             return player_values_sum / len(best_response_node.children)
+
         elif isinstance(best_response_node, BoardCardsNode):
-            player_values_sum = 0
-            for cards in best_response_node.children:
-                new_board_cards = flatten(board_cards, cards)
-                new_player_possible_nodes = filter(lambda node: cards in node.children, player_possible_nodes)
-                new_player_possible_nodes = map(lambda node: node.children[cards], new_player_possible_nodes)
-                player_values_sum += self._get_exploitability(
-                    player_position,
-                    best_response_node.children[cards],
-                    list(new_player_possible_nodes),
-                    best_response_cards,
-                    new_board_cards)
-            return player_values_sum / len(best_response_node.children)
+            raise RuntimeError('Board cards not yet supported')
+            # player_values_sum = 0
+            # for cards in best_response_node.children:
+            #     new_board_cards = flatten(board_cards, cards)
+            #     new_player_nodes = filter(lambda node: cards in node.children, player_nodes)
+            #     new_player_nodes = map(lambda node: node.children[cards], new_player_nodes)
+
+            #     player_values_sum += self._get_exploitability(
+            #         player_position,
+            #         best_response_node.children[cards],
+            #         list(new_player_nodes),
+            #         best_response_cards,
+            #         new_board_cards)
+            # return player_values_sum / len(best_response_node.children)
+
         elif best_response_node.player == player_position:
-            strategy_sum = np.zeros(3)
-            for player_node in player_possible_nodes:
-                strategy_sum += player_node.strategy
-            player_node_strategy = strategy_sum / len(player_possible_nodes)
+            player_node_strategy = np.zeros(3)
+            for i, player_node in enumerate(player_nodes):
+                player_node_strategy += np.array(player_node.strategy) * player_nodes_reach_proabilities[i]
 
             values_sum = 0
             for a in best_response_node.children:
                 player_value = self._get_exploitability(
                     player_position,
                     best_response_node.children[a],
-                    list(map(lambda node: node.children[a], player_possible_nodes)),
+                    list(map(lambda node: node.children[a], player_nodes)),
+                    player_nodes_reach_proabilities * player_node_strategy[a],
+                    player_cards,
                     best_response_cards,
                     board_cards)
                 values_sum += player_value * player_node_strategy[a]
             return values_sum
+
         else:
             best_value = None
             best_value_actions = None
@@ -113,9 +125,12 @@ class BestResponse:
                 player_value = self._get_exploitability(
                     player_position,
                     best_response_node.children[a],
-                    list(map(lambda node: node.children[a], player_possible_nodes)),
+                    list(map(lambda node: node.children[a], player_nodes)),
+                    player_nodes_reach_proabilities,
+                    player_cards,
                     best_response_cards,
                     board_cards)
+
                 if (best_value is None) or (player_value < best_value):
                     best_value = player_value
                     best_value_actions = [a]

test/cfr_correctness_test.py
@@ -10,7 +10,11 @@ import matplotlib.ticker as plticker
 import acpc_python_client as acpc
 
 from cfr.main import Cfr
-from evaluation.exploitability import Exploitability
+# from evaluation.exploitability import Exploitability
+from response.best_response import BestResponse
+from evaluation.game_value import GameValue
+
+from tools.output_util import write_strategy_to_file
 
 FIGURES_FOLDER = 'test/cfr-correctness-plots'
 
@@ -18,32 +22,32 @@ KUHN_TEST_SPEC = {
     'title': 'Kuhn Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.limit.2p.game',
     'test_counts': 3,
-    'training_iterations': 800000,
-    'checkpoint_iterations': 5000
+    'training_iterations': 12000,
+    'checkpoint_iterations': 10
 }
 
 KUHN_BIGDECK_TEST_SPEC = {
     'title': 'Kuhn Bigdeck Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.bigdeck.limit.2p.game',
-    'test_counts': 1,
-    'training_iterations': 450000,
-    'checkpoint_iterations': 10000
+    'test_counts': 3,
+    'training_iterations': 12000,
+    'checkpoint_iterations': 10
 }
 
 KUHN_BIGDECK_2ROUND_TEST_SPEC = {
     'title': 'Kuhn Bigdeck 2round Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.bigdeck.2round.limit.2p.game',
-    'test_counts': 1,
-    'training_iterations': 1000,
-    'checkpoint_iterations': 1000
+    'test_counts': 3,
+    'training_iterations': 12000,
+    'checkpoint_iterations': 20
 }
 
 LEDUC_TEST_SPEC = {
     'title': 'Leduc Hold\'em Poker CFR trained strategy exploitability',
     'game_file_path': 'games/leduc.limit.2p.game',
-    'test_counts': 1,
-    'training_iterations': 1000,
-    'checkpoint_iterations': 1000
+    'test_counts': 3,
+    'training_iterations': 10,
+    'checkpoint_iterations': 1
 }
 
 
@@ -63,12 +67,13 @@ class CfrCorrectnessTests(unittest.TestCase):
     def train_and_show_results(self, test_spec):
         game = acpc.read_game_file(test_spec['game_file_path'])
 
-        exploitability = Exploitability(game)
+        # exploitability = Exploitability(game)
 
         checkpoints_count = math.ceil(
             test_spec['training_iterations'] / test_spec['checkpoint_iterations'])
         iteration_counts = np.zeros(checkpoints_count)
         exploitability_values = np.zeros([test_spec['test_counts'], checkpoints_count])
+        # exploitability_values = np.zeros([test_spec['test_counts'] * 2, checkpoints_count])
 
         for i in range(test_spec['test_counts']):
             print('%s/%s' % (i + 1, test_spec['test_counts']))
@@ -76,7 +81,12 @@ class CfrCorrectnessTests(unittest.TestCase):
             def checkpoint_callback(game_tree, checkpoint_index, iterations):
                 if i == 0:
                     iteration_counts[checkpoint_index] = iterations
-                exploitability_values[i][checkpoint_index] = exploitability.evaluate(game_tree)
+                # exploitability_values[i * 2][checkpoint_index] = exploitability.evaluate(game_tree)
+
+                best_response = BestResponse(game).solve(game_tree)
+                game_values = GameValue(game).evaluate(game_tree, best_response)
+                exploitability_values[i][checkpoint_index] = game_values[0] * -1000
+                # exploitability_values[i * 2 + 1][checkpoint_index] = game_values[0] * -1000
 
             Cfr(game).train(
                 test_spec['training_iterations'],
@@ -85,12 +95,16 @@ class CfrCorrectnessTests(unittest.TestCase):
 
             plt.figure()
             for i in range(i + 1):
-                plt.plot(iteration_counts, exploitability_values[i])
+                plt.plot(iteration_counts, exploitability_values[i], linewidth=0.8)
+            # for i in range((i + 1) * 2):
+            #     method = 'Exploitability' if i % 2 == 0 else 'Best response + Game value'
+            #     plt.plot(iteration_counts, exploitability_values[i], label='%s %s' % (method, math.ceil((i + 1) / 2)))
 
             plt.title(test_spec['title'])
             plt.xlabel('Training iterations')
             plt.ylabel('Strategy exploitability [mbb/g]')
             plt.grid()
+            # plt.legend()
 
             game_name = test_spec['game_file_path'].split('/')[1][:-5]
             figure_output_path = '%s/%s.png' % (FIGURES_FOLDER, game_name)