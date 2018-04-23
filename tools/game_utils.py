cba19fbadf8d69b567255a8282974749aff57835
Jakub Petriska <jakubpetriska@gmail.com>
2018-04-23 19:41:13 +0200

    Add method to calculate hole card combinations in game

---
+++ tools/game_utils.py
@@ -0,0 +1,10 @@
+import scipy.misc
+import scipy.special
+
+
+def get_num_hole_card_combinations(game):
+    num_players = game.get_num_players()
+    num_hole_cards = game.get_num_hole_cards()
+    num_cards = game.get_num_suits() * game.get_num_ranks()
+    num_total_hole_cards = num_players * num_hole_cards
+    return scipy.misc.comb(num_cards, num_total_hole_cards) * scipy.special.perm(num_total_hole_cards, num_total_hole_cards)
