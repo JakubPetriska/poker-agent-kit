ea05f5aa0060591fd35a26d1ff46254f47edd761
Jakub Petriska <jakubpetriska@gmail.com>
2018-04-24 11:33:16 +0200

    Improve cfr correctness tests

--- test/cfr_correctness_test.py
+++ test/cfr_correctness_test.py
@@ -11,31 +11,33 @@ import acpc_python_client as acpc
 
 from cfr.main import Cfr
 from evaluation.exploitability import Exploitability
+from response.best_response import BestResponse
+from evaluation.game_value import GameValue
 
 from tools.output_util import write_strategy_to_file
 
-FIGURES_FOLDER = 'test/cfr-correctness-plots'
+FIGURES_FOLDER = 'test/cfr_correctness'
 
 KUHN_TEST_SPEC = {
     'title': 'Kuhn Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.limit.2p.game',
-    'test_counts': 1,
-    'training_iterations': 12000,
-    'checkpoint_iterations': 20
+    'test_counts': 2,
+    'training_iterations': 50000,
+    'checkpoint_iterations': 10
 }
 
 KUHN_BIGDECK_TEST_SPEC = {
     'title': 'Kuhn Bigdeck Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.bigdeck.limit.2p.game',
-    'test_counts': 3,
-    'training_iterations': 12000,
-    'checkpoint_iterations': 20
+    'test_counts': 1,
+    'training_iterations': 20000,
+    'checkpoint_iterations': 10
 }
 
 KUHN_BIGDECK_2ROUND_TEST_SPEC = {
     'title': 'Kuhn Bigdeck 2round Poker CFR trained strategy exploitability',
     'game_file_path': 'games/kuhn.bigdeck.2round.limit.2p.game',
-    'test_counts': 3,
+    'test_counts': 1,
     'training_iterations': 12000,
     'checkpoint_iterations': 20
 }
@@ -43,9 +45,9 @@ KUHN_BIGDECK_2ROUND_TEST_SPEC = {
 LEDUC_TEST_SPEC = {
     'title': 'Leduc Hold\'em Poker CFR trained strategy exploitability',
     'game_file_path': 'games/leduc.limit.2p.game',
-    'test_counts': 3,
-    'training_iterations': 20000,
-    'checkpoint_iterations': 20
+    'test_counts': 1,
+    'training_iterations': 1000,
+    'checkpoint_iterations': 10
 }
 
 
@@ -70,7 +72,7 @@ class CfrCorrectnessTests(unittest.TestCase):
         checkpoints_count = math.ceil(
             test_spec['training_iterations'] / test_spec['checkpoint_iterations'])
         iteration_counts = np.zeros(checkpoints_count)
-        exploitability_values = np.zeros([test_spec['test_counts'] * 2, checkpoints_count])
+        exploitability_values = np.zeros([test_spec['test_counts'], checkpoints_count])
 
         for i in range(test_spec['test_counts']):
             print('%s/%s' % (i + 1, test_spec['test_counts']))
@@ -78,35 +80,36 @@ class CfrCorrectnessTests(unittest.TestCase):
             def checkpoint_callback(game_tree, checkpoint_index, iterations):
                 if i == 0:
                     iteration_counts[checkpoint_index] = iterations
-                exploitability_values[i * 2:(i * 2) + 2, checkpoint_index] = exploitability.evaluate(game_tree)
+                exploitability_values[i, checkpoint_index] = exploitability.evaluate(game_tree)
 
-                # best_response = BestResponse(game).solve(game_tree)
-                # game_values = GameValue(game).evaluate(game_tree, best_response)
-                # exploitability_values[i][checkpoint_index] = game_values[0] * -1000
-                # exploitability_values[i * 2 + 1][checkpoint_index] = game_values[0] * -1000
-
-            Cfr(game).train(
+            cfr = Cfr(game)
+            cfr.train(
                 test_spec['training_iterations'],
                 test_spec['checkpoint_iterations'],
-                checkpoint_callback)
+                checkpoint_callback,
+                minimal_action_probability=0.00004)
+
+            best_response = BestResponse(game).solve(cfr.game_tree)
+            game_values, _ = GameValue(game).evaluate(cfr.game_tree, best_response)
+            print(game_values.tolist())
+
+            write_strategy_to_file(cfr.game_tree, 'test/cfr_correctness/cfr.strategy')
 
             plt.figure(dpi=160)
-            # for i in range(i + 1):
-            #     plt.plot(iteration_counts, exploitability_values[i], linewidth=0.8)
-            for j in range((i + 1) * 2):
+            for j in range(i + 1):
                 run_index = math.floor(j / 2)
-                player_position = j % 2
                 plt.plot(
                     iteration_counts,
                     exploitability_values[j],
-                    label='Run %s, player position %s' % (run_index + 1, player_position),
+                    label='Run %s' % (run_index + 1),
                     linewidth=0.8)
 
             plt.title(test_spec['title'])
             plt.xlabel('Training iterations')
             plt.ylabel('Strategy exploitability [mbb/g]')
             plt.grid()
-            plt.legend()
+            if test_spec['test_counts'] > 1:
+                plt.legend()
 
             game_name = test_spec['game_file_path'].split('/')[1][:-5]
             figure_output_path = '%s/%s.png' % (FIGURES_FOLDER, game_name)
