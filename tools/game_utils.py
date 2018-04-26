685365af5126c6e83db468eef24b008fc1526462
Jakub Petriska <jakubpetriska@gmail.com>
2018-04-26 18:07:22 +0200

    Add method to verify that all strategy probabilities add to 1

--- tools/game_utils.py
+++ tools/game_utils.py
@@ -1,5 +1,9 @@
+import numpy as np
 import scipy.misc
 import scipy.special
+
+from tools.walk_tree import walk_tree
+from tools.game_tree.nodes import ActionNode
 
 
 def get_num_hole_card_combinations(game):
@@ -9,3 +13,15 @@ def get_num_hole_card_combinations(game):
     num_total_hole_cards = num_players * num_hole_cards
     return scipy.misc.comb(num_cards, num_total_hole_cards, exact=True) \
         * scipy.special.perm(num_total_hole_cards, num_total_hole_cards, exact=True)
+
+
+def is_correct_strategy(strategy_tree):
+    correct = True
+    def on_node(node):
+        if isinstance(node, ActionNode):
+            nonlocal correct
+            strategy_sum = np.sum(node.strategy)
+            if strategy_sum != 1:
+                correct = False
+    walk_tree(strategy_tree, on_node)
+    return correct
