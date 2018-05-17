1f27b3dc4ae60dfb80450b89a1f50aa5df4589bf
Jakub Petriska <jakubpetriska@gmail.com>
2018-05-17 22:18:52 +0200

    Big imaginary observations refactor

--- utility_estimation/imaginary_observations.py
+++ utility_estimation/imaginary_observations.py
@@ -9,71 +9,81 @@ from utility_estimation.utils import get_all_board_cards, get_board_cards
 
 
 class ImaginaryObservationsUtilityEstimator():
-    def __init__(self, game, portfolio_strategies):
+    def __init__(self, game, mucking_enabled):
         if game.get_num_players() != 2:
             raise AttributeError(
                 'Only games with 2 players are supported')
 
         self.game = game
-        self.portfolio_strategies = portfolio_strategies
-        self.num_experts = len(portfolio_strategies)
+        self.mucking_enabled = mucking_enabled
 
-    def get_expert_utility_estimations(self, match_state, expert_probabilities):
+    def get_utility_estimations(self, state, player, sampling_strategy, evaluated_strategies=None):
+        if evaluated_strategies is None:
+            evaluated_strategies = [sampling_strategy]
+
         num_players = self.game.get_num_players()
-        player = match_state.get_viewing_player()
         opponent_player = (player + 1) % 2
-        state = match_state.get_state()
 
-        expert_utilities = np.zeros(self.num_experts)
+        num_evaluated_strategies = len(evaluated_strategies)
+
+        utilities = np.zeros(num_evaluated_strategies)
 
         any_player_folded = False
         for p in range(num_players):
             any_player_folded = any_player_folded or state.get_player_folded(p)
+
         all_board_cards = get_all_board_cards(self.game, state)
 
-        hole_cards_node = self.portfolio_strategies[0]
         opponent_hole_cards = None
         possible_player_hole_cards = None
-        if any_player_folded:
+        if any_player_folded and self.mucking_enabled:
             possible_player_hole_cards = list(filter(
                 lambda hole_cards: is_unique(hole_cards, all_board_cards),
-                hole_cards_node.children))
+                sampling_strategy.children))
         else:
             opponent_hole_cards = [state.get_hole_card(opponent_player, c) for c in range(self.game.get_num_hole_cards())]
             possible_player_hole_cards = list(filter(
                 lambda hole_cards: is_unique(hole_cards, opponent_hole_cards, all_board_cards),
-                hole_cards_node.children))
-        nodes = [[expert_node.children[hole_cards] for hole_cards in possible_player_hole_cards] for expert_node in self.portfolio_strategies]
-        num_nodes = len(possible_player_hole_cards)
-        expert_reach_probabilities = np.ones([self.num_experts, num_nodes])
-        player_reach_probabilities = np.ones(num_nodes)
+                sampling_strategy.children))
+        nodes = [
+            [
+                expert_node.children[hole_cards]
+                for hole_cards in possible_player_hole_cards]
+            for expert_node in evaluated_strategies]
+        sampling_strategy_nodes = [sampling_strategy.children[hole_cards] for hole_cards in possible_player_hole_cards]
 
-        def add_terminals_to_utilities(nodes, players_folded, expert_reach_probabilities, player_reach_probabilities):
-            nonlocal expert_utilities
+        num_nodes = len(possible_player_hole_cards)
+        evaluated_strategies_reach_probabilities = np.ones([num_evaluated_strategies, num_nodes])
+        sampling_strategy_reach_probabilities = np.ones(num_nodes)
+
+        def add_terminals_to_utilities(pot_commitment, players_folded, sampling_strategy_reach_probabilities, evaluated_strategies_reach_probabilities):
+            nonlocal utilities
             nonlocal player
-            player_reach_probability = np.sum(player_reach_probabilities)
+
+            sampling_strategy_reach_probability_sum = np.sum(sampling_strategy_reach_probabilities)
+            if sampling_strategy_reach_probability_sum == 0:
+                return
+
             for i in range(num_nodes):
                 utility = None
                 if players_folded[player]:
-                    utility = -nodes[0][0].pot_commitment[player]
+                    utility = -pot_commitment[player]
                 else:
                     hole_cards = [possible_player_hole_cards[i] if p == player else opponent_hole_cards for p in range(num_players)]
                     utility = get_utility(
                         hole_cards,
                         all_board_cards,
                         players_folded,
-                        nodes[0][0].pot_commitment)[match_state.get_viewing_player()]
-                expert_utilities += utility * (expert_reach_probabilities[:, i] / player_reach_probability)
+                        pot_commitment)[player]
+                utilities += utility * (evaluated_strategies_reach_probabilities[:, i] / sampling_strategy_reach_probability_sum)
 
-        def update_reach_proabilities(action, nodes, expert_reach_probabilities, player_reach_probabilities):
+        def update_reach_proabilities(action, sampling_strategy_nodes, nodes, sampling_strategy_reach_probabilities, evaluated_strategies_reach_probabilities):
             for i in range(num_nodes):
-                player_reach_prob_multiplier = 0
-                for j in range(self.num_experts):
-                    expert_action_probability = nodes[j][i].strategy[action]
-                    expert_reach_probabilities[j][i] *= expert_action_probability
-                    player_reach_prob_multiplier += expert_action_probability * expert_probabilities[j]
-                if player_reach_probabilities is not None:
-                    player_reach_probabilities[i] *= player_reach_prob_multiplier
+                if sampling_strategy_reach_probabilities is not None:
+                    sampling_strategy_reach_probabilities[i] *= sampling_strategy_nodes[i].strategy[action]
+
+                for j in range(num_evaluated_strategies):
+                    evaluated_strategies_reach_probabilities[j, i] *= nodes[j][i].strategy[action]
 
         round_index = 0
         action_index = 0
@@ -82,28 +92,44 @@ class ImaginaryObservationsUtilityEstimator():
             if isinstance(node, BoardCardsNode):
                 new_board_cards = get_board_cards(self.game, state, round_index)
                 nodes = [[expert_node.children[new_board_cards] for expert_node in expert_nodes] for expert_nodes in nodes]
+                sampling_strategy_nodes = [node.children[new_board_cards] for node in sampling_strategy_nodes]
             elif isinstance(node, ActionNode):
                 action = convert_action_to_int(state.get_action_type(round_index, action_index))
                 if node.player == player:
-                    for other_action in node.children:
-                        if other_action != action and isinstance(node.children[other_action], TerminalNode) \
-                                and (other_action == 0 or not any_player_folded):
-                            expert_reach_probabilities_copy = np.copy(expert_reach_probabilities)
-                            update_reach_proabilities(0, nodes, expert_reach_probabilities_copy, None)
-                            players_folded = [True if p == player and other_action == 0 else False for p in range(2)]
-                            new_nodes = [[expert_node.children[other_action] for expert_node in expert_nodes] for expert_nodes in nodes]
-                            add_terminals_to_utilities(new_nodes, players_folded, expert_reach_probabilities_copy, player_reach_probabilities)
-                    else:
-                        update_reach_proabilities(action, nodes, expert_reach_probabilities, player_reach_probabilities)
+                    # for other_action in filter(lambda a: a != action and isinstance(node.children[a], TerminalNode), node.children):
+                    #     sampling_strategy_reach_probabilities_copy = np.copy(sampling_strategy_reach_probabilities)
+                    #     evaluated_strategies_reach_probabilities_copy = np.copy(evaluated_strategies_reach_probabilities)
+                    #     update_reach_proabilities(
+                    #         other_action,
+                    #         sampling_strategy_nodes,
+                    #         nodes,
+                    #         sampling_strategy_reach_probabilities_copy,
+                    #         evaluated_strategies_reach_probabilities_copy)
+
+                    #     players_folded = [True if p == player and other_action == 0 else False for p in range(2)]
+
+                    #     add_terminals_to_utilities(
+                    #         node.children[other_action].pot_commitment,
+                    #         players_folded,
+                    #         sampling_strategy_reach_probabilities,
+                    #         evaluated_strategies_reach_probabilities_copy)
+
+                    update_reach_proabilities(
+                        action,
+                        sampling_strategy_nodes,
+                        nodes,
+                        sampling_strategy_reach_probabilities,
+                        evaluated_strategies_reach_probabilities)
 
                 action_index += 1
                 if action_index == state.get_num_actions(round_index):
                     round_index += 1
                     action_index = 0
                 nodes = [[expert_node.children[action] for expert_node in expert_nodes] for expert_nodes in nodes]
+                sampling_strategy_nodes = [node.children[action] for node in sampling_strategy_nodes]
             elif isinstance(node, TerminalNode):
                 players_folded = [state.get_player_folded(p) for p in range(num_players)]
-                add_terminals_to_utilities(nodes, players_folded, expert_reach_probabilities, player_reach_probabilities)
+                add_terminals_to_utilities(node.pot_commitment, players_folded, sampling_strategy_reach_probabilities, evaluated_strategies_reach_probabilities)
                 break
 
-        return expert_utilities
+        return utilities
